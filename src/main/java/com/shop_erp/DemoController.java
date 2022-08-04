c6aaf0e11fb418d1dd7b21a965efbdcd6fe733ad
songhuisu <thdtn816@gmail.com>
2022-08-04 21:22:57 +0900

    커밋

---
+++ src/main/java/com/shop_erp/DemoController.java
@@ -0,0 +1,13 @@
+package com.shop_erp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "home";
+    }
+}
